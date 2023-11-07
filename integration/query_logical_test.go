bf036b7cedcf2067dc641ceecb6f722a7aeea440
Elena Grahovac <elena@grahovac.me>
2023-11-07 10:53:52 +0100

    update test

--- integration/query_logical_test.go
+++ integration/query_logical_test.go
@@ -20,34 +20,28 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/FerretDB/FerretDB/integration/setup"
-	"github.com/FerretDB/FerretDB/integration/shareddata"
 )
 
-// Run against MongoDB (from integration directory):
-// go test -count=1 -run='TestQueryLogicalAnd' -target-url=mongodb://127.0.0.1:47017/ -target-backend=mongodb .
-//
-// Run against FerretDB:
-// go test -count=1 -run='TestQueryLogicalAnd' -target-backend=ferretdb-pg -target-tls -postgresql-url=postgres://username@127.0.0.1:5432/ferretdb .
+// Run against FerretDB (from integration directory):
+// task test-integration-postgresql TEST_RUN="TestQueryLogicalAnd"
 func TestQueryLogicalAnd(t *testing.T) {
 	t.Parallel()
 
-	ctx, collection := setup.Setup(t, shareddata.Int32s)
+	ctx, collection := setup.Setup(t, Int32s)
 
-	// Inserted data - shareddata.Int32s (sorted by v):
-	// {_id: "int32-min",  v: -2147483648}, // math.MinInt32
-	// {_id: "int32-zero", v: 0},
-	// {_id: "int32-1",    v: 1},
-	// {_id: "int32",      v: 42},
-	// {_id: "int32-max",  v: 2147483647}, // math.MaxInt32
+	// Inserted data - Int32s (sorted by v):
+	// {{"_id", "int32-min"},  {"v", int32(math.MinInt32)}}, // -2147483648
+	// {{"_id", "int32-zero"}, {"v", int32(0)}},
+	// {{"_id", "int32-1"},    {"v", int32(1)}},
+	// {{"_id", "int32"},      {"v", int32(42)}},
+	// {{"_id", "int32-max"},  {"v", int32(math.MaxInt32)}}, // 2147483647
 
 	for name, tc := range map[string]struct {
-		filter bson.D             // required, filter to be tested
-		res    []bson.D           // expected result
-		err    mongo.CommandError // expected error
+		filter bson.D   // required, filter to be tested
+		res    []bson.D // expected result
 	}{
 		"Two": {
 			// {$and: [{v: {$gt: 0}}, {v: {$lt: 42}}]}
@@ -58,73 +52,7 @@ func TestQueryLogicalAnd(t *testing.T) {
 				},
 			}},
 			res: []bson.D{
-				{
-					{"_id", "int32-1"},
-					{"v", int32(1)},
-				},
-			},
-		},
-		"One": {
-			// {$and: [{v: {$gt: 0}}]}
-			filter: bson.D{{
-				"$and", bson.A{
-					bson.D{{"v", bson.D{{"$gt", int32(0)}}}},
-				},
-			}},
-			res: []bson.D{
-				{
-					{"_id", "int32-1"},
-					{"v", int32(1)},
-				},
-				{
-					{"_id", "int32"},
-					{"v", int32(42)},
-				},
-				{
-					{"_id", "int32-max"},
-					{"v", int32(math.MaxInt32)},
-				},
-			},
-		},
-		"AndAnd": {
-			// {$and: [{$and: [{v: {$gt: 0}}, {v: {$lt: 42}}]}, {v: {$type: "int"}}]}
-			filter: bson.D{{
-				"$and", bson.A{
-					bson.D{{"$and", bson.A{
-						bson.D{{"v", bson.D{{"$gt", int32(0)}}}},
-						bson.D{{"v", bson.D{{"$lte", 42.13}}}},
-					}}},
-					bson.D{{"v", bson.D{{"$type", "int"}}}},
-				},
-			}},
-			res: []bson.D{
-				{
-					{"_id", "int32-1"},
-					{"v", int32(1)},
-				},
-				{
-					{"_id", "int32"},
-					{"v", int32(42)},
-				},
-			},
-		},
-		"Zero": {
-			// {$and: []}
-			filter: bson.D{{
-				"$and", bson.A{},
-			}},
-			err: mongo.CommandError{
-				Code:    2,
-				Name:    "BadValue",
-				Message: "$and/$or/$nor must be a nonempty array",
-			},
-		},
-		"BadInput": {
-			filter: bson.D{{"$and", nil}},
-			err: mongo.CommandError{
-				Code:    2,
-				Name:    "BadValue",
-				Message: "$and must be an array",
+				{{"_id", "int32-1"}, {"v", int32(1)}},
 			},
 		},
 	} {
@@ -133,18 +61,40 @@ func TestQueryLogicalAnd(t *testing.T) {
 			t.Parallel()
 
 			opts := options.Find().SetSort(bson.D{{"v", 1}})
-
 			cursor, err := collection.Find(ctx, tc.filter, opts)
-
-			if tc.res == nil {
-				AssertEqualCommandError(t, tc.err, err)
-				return
-			}
+			require.NoError(t, err)
 
 			res := FetchAll(t, ctx, cursor)
-
 			require.NoError(t, err)
 			AssertEqualDocumentsSlice(t, tc.res, res)
 		})
 	}
+}
+
+// Values stores _id and value pair.
+type Values map[string]any
+
+// Int32s contains int32 values for tests.
+var Int32s Values = map[string]any{
+	"int32-min":  int32(math.MinInt32),
+	"int32-zero": int32(0),
+	"int32-1":    int32(1),
+	"int32":      int32(42),
+	"int32-max":  int32(math.MaxInt32),
+}
+
+// Name implement Provider interface.
+func (vs Values) Name() string {
+	return "Int32s"
+}
+
+// Docs implement Provider interface.
+func (vs Values) Docs() []bson.D {
+	res := make([]bson.D, 0, len(vs))
+
+	for id, v := range vs {
+		res = append(res, bson.D{{"_id", id}, {"v", v}})
+	}
+
+	return res
 }
